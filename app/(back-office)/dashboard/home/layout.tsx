6d063bf816cdecb04c4f6f256ddc9eeeee5eb3cd
AntonCol <colaizziantonio@virgilio.it>
2024-09-30 12:18:40 +0200

    struttura progetto

---
+++ app/(back-office)/dashboard/home/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface HomeLayoutProps {
+  children: ReactNode;
+}
+
+export default function HomeLayout({ children }: HomeLayoutProps) {
+  return <div>layout</div>;
+}
